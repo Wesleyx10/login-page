90e2fd05c8ad01bf10c988b97ff55fc2b55edb58
Wesley <wesleyorobosa73@gmail.com>
2025-04-05 23:40:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const signup = document.getElementById("sign");
+
+  if (signup) {
+    signup.addEventListener("click", () => {
+      window.location.href = "2index.html";
+    });
+  }
+});
